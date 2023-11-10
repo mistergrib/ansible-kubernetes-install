e4978058039ce7c4619802d0822e179e3015938c
mistergrib <54153868+mistergrib@users.noreply.github.com>
2023-11-10 17:49:45 +0300

    Create initial-install-rocky.sh

---
+++ initial-install-rocky.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+
+#-------vars block----------------
+newuser=ansible
+pass=<pass>
+pubkey=<ssh_key>
+#--------add ansible user------------
+useradd -m -s /bin/bash $newuser
+usermod -aG wheel $newuser
+echo "$newuser:$pass" | chpasswd
+echo -e "ansible ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+#--------add ssh key for ansible user---------
+mkdir /home/$newuser/.ssh
+chmod 700 /home/$newuser/.ssh
+touch /home/$newuser/.ssh/authorized_keys
+chmod 600 /home/$newuser/.ssh/authorized_keys
+chown -R $newuser /home/$newuser/.ssh
+chgrp -R $newuser /home/$newuser/.ssh
+echo "$pubkey">> /home/$newuser/.ssh/authorized_keys
+sed -i 's|[#]*PasswordAuthentication yes|PasswordAuthentication no|g' /etc/ssh/sshd_config
+sed -i 's|[#]*PermitRootLogin yes|PermitRootLogin no|g' /etc/ssh/sshd_config
+systemctl restart sshd
